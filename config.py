4f6c4e22e999bfab6afcb91647758fb1f4e027a8
byaskew3 <byaskew3@gmail.com>
2022-06-21 15:23:20 -0600

    db

--- config.py
+++ config.py
@@ -13,5 +13,5 @@ class Config():
     FLASK_APP = os.getenv('FLASK_APP')
     FLASK_ENV = os.getenv('FLASK_ENV')
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'Brandon always hates on JS'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_NOTIFICATIONS = False
